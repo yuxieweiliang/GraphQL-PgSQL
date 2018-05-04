dc373d57f503a436a1b9dce624de95ebf27e4fc0
风吹雪 <yunruofengsheng@126.com>
2018-05-04 18:52:19 +0800

    first commit

---
+++ client.js
@@ -0,0 +1,14 @@
+
+const { Client } = require('pg')
+
+// const client = new Client('tcp://postgres:xyf.3342@localhost/postgres')
+const client = new Client({
+  user: "postgres",
+  password: "xyf.3342",
+  database: "postgres",
+  host: "127.0.0.1",
+  port: 5433
+})
+client.connect()
+
+module.exports = client
---
+++ server.js
@@ -0,0 +1,68 @@
+// 框架
+var Koa = require('koa')
+// path
+var path = require('path')
+// 跨域
+var cors = require('koa2-cors')
+// 路由
+var Router = require('koa-router')
+
+var client = require('./client');
+
+var graphql = require('graphql');
+
+var { graphqlKoa, graphiqlKoa } = require('graphql-server-koa');
+
+const { Client } = require('pg')
+
+const app = new Koa();
+
+const router = new Router();
+
+// const client = new Client();
+
+// 获取项目根目录
+const ROOTS = process.cwd();
+
+// 跨域
+app.use(cors({
+  origin: function(ctx) {
+    return '*';
+  }})
+);
+
+
+
+router.get('/', async function(ctx) {
+  client.query('select * from student',  function(error, results) {
+
+    console.log(error, results)
+
+  });
+  client.query(`CREATE TABLE teacher
+  (
+  id integer NOT NULL,
+  "name" character(100),
+  subjects character(1),
+  CONSTRAINT teacher_key PRIMARY KEY (id)
+)`,  function(error, results) {
+
+    console.log(error, results)
+
+  });
+
+  console.log('-----------')
+
+  ctx.body = 'fffffffffffffffffffffffffff'
+})
+/*router.get('/graphql', async (ctx, next) => {
+  await graphiqlKoa({endpointURL: '/graphql'})(ctx, next)
+})*/
+
+app.use(router.routes());
+app.listen(8989);
+
+
+
+
+
